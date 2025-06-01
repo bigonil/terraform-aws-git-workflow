4eb965d68d098fbb465210bc3caec5c57b7db1a2
Bigoni Luca <luca.bigoni@gruppohera.it>
2025-06-01 15:20:08 +0200

    Add source  = bigonil/apache/aws

--- mainf.tf
+++ mainf.tf
@@ -15,7 +15,8 @@ provider "aws" {
 }
 
 module "apache" {
-    source             = "./terraform-aws-apache-example"
+    source  = "bigonil/apache/aws"
+    version = "1.0.0"
     vpc_id          = var.vpc_id
     my_ip_with_cidr = var.my_ip_with_cidr
     public_key      = var.public_key
