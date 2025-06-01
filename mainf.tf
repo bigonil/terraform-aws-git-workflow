1f2da9f4efbbc95757e80f480a321085d5236925
Bigoni Luca <luca.bigoni@gruppohera.it>
2025-06-01 15:25:42 +0200

    Update module source name with apache-example

--- mainf.tf
+++ mainf.tf
@@ -15,13 +15,13 @@ provider "aws" {
 }
 
 module "apache-example" {
-  source          = "bigonil/apache/aws"
-  version         = "1.0.0"
-  vpc_id          = var.vpc_id
-  my_ip_with_cidr = var.my_ip_with_cidr
-  public_key      = var.public_key
-  instance_type   = var.instance_type
-  server_name     = var.server_name
+  source  = "bigonil/apache-example/aws"
+  version = "1.0.0"
+    vpc_id          = var.vpc_id
+    my_ip_with_cidr = var.my_ip_with_cidr
+    public_key      = var.public_key
+    instance_type   = var.instance_type
+    server_name     = var.server_name
 }
 
 output "public_ip" {
